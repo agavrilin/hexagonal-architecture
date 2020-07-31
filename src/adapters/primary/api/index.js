18cf02c22e529cecb61be4b3e1481e9f07034d32
Artyom Gavrilin <artyom_gavrilin@MacBook-Pro-Intergiro.local>
2020-07-31 08:43:25 +0300

    Added basic UI

--- src/adapters/primary/api/index.js
+++ src/adapters/primary/api/index.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 
-const user = require('./user/controller');
+const user = require('../../../domains/user/controller');
 
 const router = new Router();
 
 router.post('/user/signup', user.registration);
+router.get('/user/signup', user.registration);
 
 module.exports = router;
